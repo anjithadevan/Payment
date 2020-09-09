ca8ba9b89ef0dffa4c822c2e8ef1ed51d55e7855
Anjitha <anjitha.d@bridge-global.com>
2020-09-09 19:05:50 +0530

    created model

--- client_payment/models.py
+++ client_payment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    """
+    Payment Informations
+    """
+    invoice_number = models.CharField(max_length=10)
+    client_name = models.CharField(max_length=100)
+    client_email = models.EmailField(max_length=254)
+    project_name = models.CharField(max_length=100)
+    amount = models.IntegerField(null=False, blank=False)
+    short_url = models.CharField(max_length=200)
+    payment_status = models.BooleanField(default=False)
