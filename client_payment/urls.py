dd78119fdb9b654999db2e60538e63a54a52920d
Anjitha <anjitha.d@bridge-global.com>
2020-09-10 13:33:22 +0530

    integrated stripe account for payment

--- client_payment/urls.py
+++ client_payment/urls.py
@@ -4,9 +4,8 @@ from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
-# router.register(r'wallet/deposits', views.AddvirtualMoneyViewSet)
-
 urlpatterns = [
     path('', include(router.urls)),
     path('payment/<int:id>/', TemplateView.as_view(template_name="base.html")),
+    path('payments', PaymentViewSet.as_view()),
 ]
--- client_payment/views.py
+++ client_payment/views.py
@@ -1,3 +1,34 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 
-# Create your views here.
+import stripe
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from client_payment.models import Payment
+
+stripe.api_key = "sk_test_51HPWldKVHvNCtbw18iAHyPLWQg0ujNRulQe7gyMsdHTNtNanwluoXOHDHDBYptPYU6bKEKcOdRig1kATuB7fyzBF00piz1d1Xh"
+
+
+class PaymentViewSet(APIView):
+    queryset = Payment.objects.all()
+    permission_classes = (AllowAny,)
+
+    def post(self, request, format=None):
+        id = request.data['id']
+        if request.data['stripeToken']:
+            try:
+                payment = Payment.objects.get(id=id)
+                customer = stripe.Customer.create(email=payment.client_email, name=payment.client_name, source=request.data['stripeToken'])
+                charge = stripe.Charge.create(customer=customer, amount=payment.amount, currency="inr")
+                payment.payment_status = True
+                payment.save()
+                return Response('success', status=HTTP_200_OK)
+            except ObjectDoesNotExist:
+                return Response('Failed', status=HTTP_400_BAD_REQUEST)
+        return Response('Failed', status=HTTP_400_BAD_REQUEST)
+
+
